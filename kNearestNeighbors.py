2e2f71612b5c0da6c4673bd2622a782e2281f732
Mikel Kratzer <mikel.kratzer@gmail.com>
2025-04-21 13:18:14 -0600

    4/21/25

--- kNearestNeighbors.py
+++ kNearestNeighbors.py
@@ -1,17 +1,51 @@
+import psycopg2 as pg
+import matplotlib.pyplot as plt
 
+conn_string = "host = 'localhost' dbname = 'csci260' password = 'password'"
+conn = pg.connect(conn_string)
+cursor = conn.cursor()
 
-
-
-
-
-
-cursor.execute("select coalesce(total_bedrooms,0) as x, coalesce(median_income, 0) as y, coalesce(median_house_value, 0) as z from normhousing")
-
+labels = ("total_bedrooms", "median_income", "median_house_value")
+cursor.execute("select coalesce(%s,0) as x, coalesce(%s, 0) as y, coalesce(%s, 0) as z from normhousing limit 500;" %labels)
+records = cursor.fetchall()
+cols = {"x":0, "y":1, "z":2}
 
 x = []
 y = []
 z = []
 for r in records:
-    x.append(r[0])
-    y.append(r[0])
-    z.append(r[2])
+    x.append(r[cols['x']])
+    y.append(r[cols['y']])
+    z.append(r[cols['z']])
+
+goodx = [0.15]
+goody = [0.45]
+goodz = [0.75] # directly from realtor/client
+cursor.execute("select %s, %s, %s, sqrt((%s-%f)^2+(%s-%f)^2+(%s-%f)^2) as d from normhousing order by d limit 10;" 
+               %(labels[0], labels[1], labels[2], labels[0], goodx[0], labels[1], goody[0], labels[2], goodz[0]))
+closeRecords = cursor.fetchall()
+
+close_x = []
+close_y = []
+close_z = []
+for r in closeRecords:
+    close_x.append(r[cols['x']])
+    close_y.append(r[cols['y']])
+    close_z.append(r[cols['z']])
+
+xlimits = (min(x), max(x)) # or (0,1)
+ylimits = (min(y), max(y))
+zlimits = (min(z), max(z))
+
+
+fig  = plt.figure()
+ax = fig.add_subplot(projection='3d')
+
+ax.scatter(x,y,z, marker = 'o')
+ax.scatter(goodx, goody, goodz, marker = '^')
+ax.scatter(close_x, close_y, close_z, marker = '*', s = 30)
+ax.set_xlabel(labels[0])
+ax.set_ylabel(labels[1])
+ax.set_zlabel(labels[2])
+
+plt.show()
--- normHousing.sql
+++ normHousing.sql
@@ -1,31 +1,53 @@
--- remove existing tables
+-- remove extremes table
 drop table if exists extremes;
 drop table if exists normhousing;
 
--- make the normhousing table
+-- Make the normhousing table
 create table normhousing (like housing);
 
--- create extremes table
+-- Create extremes table
 create table extremes (
-    minlongitude float, maxlongitude float,
-    minlatitude float, maxlatitude float,
+    minlongitude float,maxlongitude float,
+    minlatitude float,maxlatitude float,
     minhousing_median_age float, maxhousing_median_age float,
-    mintotal_rooms float, maxtotal_rooms float,
-    mintotal_bedrooms float, maxtotal_bedrooms float,
+    mintotal_rooms float,maxtotal_rooms float,
+    mintotal_bedrooms float,maxtotal_bedrooms float,
     minpopulation float, maxpopulation float,
     minhouseholds float, maxhouseholds float,
     minmedian_income float, maxmedian_income float,
     minmedian_house_value float, maxmedian_house_value float
-    );
+  );
 
--- calculate and insert the extremes
+-- Calculate and insert the extremes  
 insert into extremes select 
-    min(longitude) as minlongitude, max(longitude) as maxlongitude 
-    from housing;
+  min(longitude) as minlongitude, max(longitude) as maxlongitude, 
+  min(latitude) as minlatitude , max(latitude) as maxlatitude ,
+  min(housing_median_age) as minhousing_median_age , max(housing_median_age) as maxhousing_median_age ,
+  min(total_rooms) as mintotal_rooms , max(total_rooms) as maxtotal_rooms ,
+  min(total_bedrooms) as mintotal_bedrooms , max(total_bedrooms) as maxtotal_bedrooms ,
+  min(population) as minpopulation , max(population) as maxpopulation ,
+  min(households) as minhouseholds , max(households) as maxhouseholds ,
+  min(median_income) as minmedian_income , max(median_income) as maxmedian_income ,
+  min(median_house_value) as minmedian_house_value , max(median_house_value) as maxmedian_house_value
+  from housing;
 
--- add normalized values to normhousing table
+-- function beUseful(field,minfield,maxfield)
+create or replace function n(value float, min float, max float) returns float
+  LANGUAGE SQL
+  IMMUTABLE
+  return (value - min) / (max - min);
+
+-- Add the normalized values to the normhousing table
 insert into normhousing select 
-    (longitude - minlongitude)/(maxlongitude-minlongitude) as longitude 
-    from housing, extremes;
+  n(longitude,minlongitude,maxlongitude) as longitude, 
+  n(latitude,minlatitude ,maxlatitude ) as  latitude, 
+  n(housing_median_age,minhousing_median_age ,maxhousing_median_age ) as  housing_median_age, 
+  n(total_rooms,mintotal_rooms ,maxtotal_rooms ) as  total_rooms, 
+  n(total_bedrooms,mintotal_bedrooms ,maxtotal_bedrooms ) as  total_bedrooms, 
+  n(population,minpopulation ,maxpopulation ) as  population, 
+  n(households,minhouseholds ,maxhouseholds ) as  households, 
+  n(median_income,minmedian_income ,maxmedian_income ) as  median_income, 
+  n(median_house_value,minmedian_house_value ,maxmedian_house_value ) as  median_house_value
+  from housing, extremes;
 
 -- to run, type into PSQL: "\i normHousing.sql"
